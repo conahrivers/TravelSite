cb991551390482a359b4c0e649ed40855affc3ea
conahrivers <conahcharlierivers@gmail.com>
2017-12-10 11:17:58 +0000

    Update transport_script.js

--- Transport website/javascript/transport_script.js
+++ Transport website/javascript/transport_script.js
@@ -1,78 +1,15 @@
-//script
+$(document).ready(function() {
+    
+    $.getJSON("https://transportapi.com/v3/uk/train/station/NRW/2017-12-06/12:49/timetable.json?app_id=f6b90e14&app_key=419e8a35c994665a778df9d81b960f2f&train_status=passenger",
+        function (data){
 
-
-
-$(function getTravelInfo() {
-	var transAPI = {
-		query: 'euston',
-		type: 'train_station',
-		appId: 'f6b90e14',
-		appKey: '419e8a35c994665a778df9d81b960f2f',
-		path: 'http://transportapi.com/v3/uk/places.json'
-	};
-	var station = {	// for info
-		request_time: '1970-01-01T00:00:00+00:00',
-		source: '',
-		acknowledgements: '',
-		type: '',
-		name: '',
-		latitude: 0,
-		longitude: 0,
-		accuracy: 0,
-		station_code: '',
-		tiploc_code: ''
-	};
-	var seriesPath = 'http://transportapi.com/v3/uk/places.json?query=euston&type=train_station&app_id=f6b90e14&app_key=419e8a35c994665a778df9d81b960f2f';
-
-	getPlaceData(seriesPath, station);
-	function showdata(){
-		station = getObject('allData');
-		$('#submitButton').append('<p>'+station.tiploc_code+' Station is at lat '+station.latitude+' long '+station.longitude+'</p>');
-	};
-
-
-
-	//http://transportapi.com/v3/uk/places.json?query=euston&type=train_station&app_id=f6b90e14&app_key=419e8a35c994665a778df9d81b960f2f
-
-
-	function getPlaceData(seriesPath, station){
-	// fetch the series data from the server
-		console.log('getting '+seriesPath);
-		$.getJSON(seriesPath, function (data) {
-			console.log(seriesPath, JSON.stringify(data));
-			clearObject('allData');
-			setObject('allData', data)
-			// could make a function call here or just code to execute
-			 displayResult(data);
-		})
-		.fail(function(jqXHR, textStatus, errorThrown) { 
-			console.log('getJSON request failed! ' + textStatus); 
-		});	
-	};
-
+        console.log(data);
+        });
 });
 
-function displayResult(data){
-	var items = [];
-	$.each( data, function( key, val ) {
-		if(isObject(val)){
-			 displayResult(val);
-		} else {
-			items.push( "<li id='" + key + "'>" + val + "</li>" );
-		}
-	});
-
-	$( "<ul/>", {
-		"class": "my-new-list",
-		html: items.join( "" )
-	 }).appendTo( "body" ); 
-};
-
-function isObject (item) {
-  return (typeof item === "object" && !Array.isArray(item) && item !== null);
-}
-
-
-//var apiKey = "419e8a35c994665a778df9d81b960f2f";
-	
-});
+        
+    /* 		 data.departures.all.forEach(departure => {
+                const destination = departure.destination_name;
+                const time = departure.aimed_departure_time;
+                $('.departures').append('<li><b>Going to:</b> ' + destination + 
+    '<br><b>At: </b>' + time + '</li>');  */

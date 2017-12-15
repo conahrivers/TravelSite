e623a114c00a1634918172a75bf3c868489cedeb
conahrivers <conahcharlierivers@gmail.com>
2017-12-15 09:16:47 +0000

    Update transport_script.js

--- Transport website/javascript/transport_script.js
+++ Transport website/javascript/transport_script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
 
-    $("#normalFont").hide();
+    //$("#normalFont").hide();
 
     $("#submitButton").on('click', function (e) {
 
         e.preventDefault();
 
-        var date = $("#calendarDropDown").val();
-        var time = $("#timeDropDown").val();
+        var date = $("#calendarBox").val();
+        var time = $("#calendarBox").val();
 
         if (date == "" && time == "") {
             var transport_url = 'https://transportapi.com/v3/uk/train/station/NRW/' + date + '/' + time + '/timetable.json?app_id=f6b90e14&app_key=419e8a35c994665a778df9d81b960f2f&train_status=passenger';
@@ -19,6 +19,7 @@ $(document).ready(function () {
 
                 for (var i = 0; i < departuresObject.length; i++) {
                     console.log(departuresObject[i].destination_name);
+                    console.log(data);
                 }
             });
 
@@ -30,9 +31,9 @@ $(document).ready(function () {
     });
 
     $('#datetimepicker').datetimepicker({
-        format: 'dd/MM/yyyy hh:mm:ss',
-        language: 'pt-BR'
-      });
+        format: 'yyyy/MM/dd hh:mm',
+        language: 'en-GB'
+    });
 
     $("#plusButton").on('click', function () {
         $("#normalFont").show();
@@ -45,9 +46,7 @@ $(document).ready(function () {
             'font-size': '0.98em'
         });
     });
-    $("#greenButton").on('click', function () {
-        $(".greenColour").css({
-            'background-color': 'green'
-        });
+    $('#greenButton').click(function () {
+        $('*').css('background-color', 'rgba(0, 180, 204, 0.178)');
     });
 });
